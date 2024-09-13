028767cb86d9a0e81aa27bef706b1ee22325ef14
enmael25 <enmael25@gmail.com>
2024-09-13 15:44:20 +0900

    20240913 : 도로 오브젝트 이동및 위치 조정 시스템

---
+++ RunGame/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RoadManager : MonoBehaviour
+{
+    //[SerializeField] List <GameObject>list;
+    //[SerializeField] float speed = 2;
+    //float pos;
+
+    [SerializeField] List<GameObject> roads;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] int createCount = 4;
+    [SerializeField] float offset = 40.0f;
+
+    private void Awake()
+    {
+        //list = new List<GameObject>();
+    }
+    private void Start()
+    {
+        roads.Capacity = 10;
+        AddRod();
+    }
+
+    private void Update() //맵이 움직인다.
+    {
+        for (int i = 0; i < roads.Count; i++)
+        {
+            roads[i].transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //Debug.Log("1");
+            newPosition();
+        }
+    }
+
+    void AddRod() //자동으로 리스트에 넣는 방법 
+    {
+        for (int i = 0; i < createCount; i++)
+        {
+            roads.Add(transform.GetChild(i).gameObject);
+        }
+    }
+    void newPosition()
+    {
+        //GameObject game = roads[0];
+        //roads.RemoveAt(0);
+
+        //Vector3 vector = roads[3].transform.position;
+        //Vector3 vector1 = new Vector3(0, 0, 40);
+
+
+        //game.transform.position = vector + vector1;
+
+        //roads.Add(game);
+
+        #region 강사님 풀이 
+
+        GameObject newRoad = roads[0];
+        roads.Remove(newRoad);
+
+        float newZ = roads[roads.Count-1].transform.position.z + offset;
+
+        newRoad.transform.position = new Vector3(0, 0, newZ);
+
+        roads.Add(newRoad);
+        #endregion
+    }
+}
